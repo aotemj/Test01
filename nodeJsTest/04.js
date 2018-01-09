359878d196f8c995eeba7b2a6e754fdb0c4f7db7
zhaoxinlei <991807272@qq.com>
2018-01-09 17:22:43 +0800

    #新增 nodeTest # 常用的buffer静态方法

--- nodeJsTest/04.js
+++ nodeJsTest/04.js
@@ -1,16 +1,53 @@
 //buffer基本操作
 //实例化buffer对象：
 
-let buf = new Buffer(5); //不推荐使用
-console.log(buf);
+// let buf = new Buffer(5); //不推荐使用
+// console.log(buf);
 
-let buf1 = Buffer.alloc(5);// 推荐使用
-console.log(buf1);
+// let buf1 = Buffer.alloc(5);// 推荐使用
+// console.log(buf1);
 
-let buf2 = Buffer.from('hello');
-// let buf2 = Buffer.from(5);//报错，这里from中只可以填写字符串，不可以填写数值
-console.log(buf2);
+// let buf2 = Buffer.from('hello');
+// // let buf2 = Buffer.from(5);//报错，这里from中只可以填写字符串，不可以填写数值
+// console.log(buf2);
 
-let buf3 = Buffer.from([0x62,0x63,0x64,0x65,0x66,0x67]);
-console.log(buf3);//62,63,64,65,66,67
-console.log(buf3.toString());//bcdefg
+// let buf3 = Buffer.from([0x62,0x63,0x64,0x65,0x66,0x67]);
+// console.log(buf3);//62,63,64,65,66,67
+// console.log(buf3.toString());//bcdefg
+
+
+// Buffer 静态方法
+//1  Buffer.isEncoding(编码格式);//验证是否支持当前编码格式
+// let buf4 = Buffer.isEncoding('utf8');
+// console.log(buf4);//true
+
+// let buf5 = Buffer.isEncoding('gbk');
+// console.log(buf5);//false
+
+// let buf6 = Buffer.isEncoding('base64');//true
+// console.log(buf6);
+
+//2.Buffer.isBuffer();//验证当前传入的实例对象是否为Buffer的实例
+// let buf7 = Buffer.from("hello");
+// let isBuf = Buffer.isBuffer(buf7);//true
+// console.log(isBuf);
+
+// let obj = {};
+// let isBuf2 = Buffer.isBuffer(obj);//false
+// console.log(isBuf2);
+
+//3.Buffer.byteLength()
+// let buf8 = Buffer.from('你好');
+// console.log(Buffer.byteLength(buf8));//6,默认使用utf8格式，一个汉字占3个字节
+
+// let buf9 = Buffer.from('你好','ascii');
+// console.log(Buffer.byteLength(buf9));//2, 使用ascii方式的时候，一个汉字占1 个字节，但此时的汉字已被改变，不推荐这种方式
+// console.log(buf9);
+// console.log(buf9.toString());//乱码
+
+//4. Buffer.concat();
+	let buf10 = Buffer.from('hello');
+	let buf11 = Buffer.from('world');
+	let buf12 = Buffer.concat([buf10,buf11]);
+	console.log(buf12);
+	console.log(buf12.toString());
--- nodeJsTest/node 基础.md
+++ nodeJsTest/node 基础.md
@@ -3,7 +3,9 @@
 	2.静态方法
 	3.实例方法
 
-####buffer对象实例化：
+- 实例化：
+	+ Buffer.from(string,'utf8'); 其中'utf8'为默认的编码格式，
+	+ Buffer.alloc(number);
 ````js
 	let buf = new Buffer(5);//不推荐使用
 
@@ -12,7 +14,38 @@
 	let buf2 = Buffer.from('hello');//这里的from中不可以填写Number类型数据
 
 	let buf3 = Buffer.from([0x62,0x63,0x64,0x65,0x66,0x67,0x68]);
+````
+- 静态方法:
+	+ Buffer.isEncoding();
+		作用： 验证是否支持某种编码格式
+````js
+		Buffer.isEncoding('utf8');//true
+		Buffer.isEncoding('gbk');//false, 不支持gbk这种编码格式
+````
+	+ Buffer.isBuffer();
+		作用： 验证传入的对象是否为Buffer的实例对象
+````js
+		let buf = Buffer.from('hello');
+		let isBuf = Buffer.isBuffer(buf);//true
+		let obj = {};
+		let isBuf2 = Buffer.isBuffer(obj);//false,obj 不是Buffer的实例对象
 
+````
+	+ Buffer.byteLength();
+		作用：返回被检测buffer实例对象的字节长度
+````js
+	let buf = Buffer.from('你好');
+	console.log(Buffer.byteLength(buf));//6，默认编码utf8格式下一个汉字占一个字节
 
+	let buf2 =Buffer.from('你好','ascii');
+	console.log(Buffer.byteLength(buf2));//2,ascii编码格式下一个汉字占一个字节，但此时的buf2中的内容已经不是汉字了，所以这种方式不推荐使用
+````
+	+ Buffer.concat();
+		作用： 拼接多个buffer实例对象
+````js
+		let buf1 = Buffer.from('hello');
+		let buf2 = Buffer.from('world');
+		let buf3 = Buffer.concat([buf1,buf2]);
+		console.log(buf3.toString());//'helloworld'
 ````
 
