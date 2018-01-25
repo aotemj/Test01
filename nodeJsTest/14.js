54dbd2029258214c76baca50c557b8c6d06c88d6
zhaoxinlei <991807272@qq.com>
2018-01-25 22:03:42 +0800

    #修改nodeJs# 练习 服务器静态资源加载

--- nodeJsTest/14.js
+++ nodeJsTest/14.js
@@ -1,32 +1,46 @@
-/*
-    初步实现静态资源功能
-*/ 
+// /*
+//     初步实现静态资源功能
+// */
 
-const http = require('http');
-const path = require('path');
+// const http = require('http');
+// const path = require('path');
+// const fs = require('fs');
+
+// let readFile = (url,res)=>{
+//     fs.readFile(path.join(__dirname,'www',url),'utf8',(err,fileContent)=>{
+//         if(err){
+//             res.end('server error');
+//         } else {
+//             res.end(fileContent);
+//         }
+//     });
+// }
+
+// http.createServer((req,res)=>{
+//     if(req.url.startsWith('/index')){
+//        readFile('index.html',res);
+//     }else if(req.url.startsWith('/about')){
+//         readFile('about.html',res)
+//     }else if(req.url.startsWith('/list')){
+//         readFile('list.html',res);
+//     }else{
+//         res.writeHead(200,{
+//             "Content-Type":'text/plain;charset=utf8'
+//         });
+//         res.end('404 页面未找到！');
+//     }
+// }).listen(3000);
+
+
+//初步实现静态资源加载
 const fs = require('fs');
-
-let readFile = (url,res)=>{
-    fs.readFile(path.join(__dirname,'www',url),'utf8',(err,fileContent)=>{
-        if(err){
-            res.end('server error');
-        } else {
-            res.end(fileContent);
-        }
-    });
-}
+const path = require('path');
+const http = require('http');
 
 http.createServer((req,res)=>{
     if(req.url.startsWith('/index')){
-       readFile('index.html',res);
-    }else if(req.url.startsWith('/about')){
-        readFile('about.html',res)
-    }else if(req.url.startsWith('/list')){
-        readFile('list.html',res);
-    }else{
-        res.writeHead(200,{
-            "Content-Type":'text/plain;charset=utf8'
-        });
-        res.end('404 页面未找到！');
+        res.
     }
-}).listen(3000);
+});
+
+
